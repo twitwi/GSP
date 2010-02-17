209b0356680e0cc0c58b2209e6f88893a112c389
negre <negre@c4bd1236-c4f7-0310-a2fe-a60683d9aa4a>
2010-02-17 14:10:45 +0000

    support for catadiotric cameras
    git-svn-id: svn+ssh://svn.ligforge.imag.fr/svnroot/gsp@30 c4bd1236-c4f7-0310-a2fe-a60683d9aa4a

---
+++ GSP/GSPModule/CatadioptricCubeMap.hpp
@@ -0,0 +1,45 @@
+#ifndef __CATADIOPTRIC_CUBE_MAP_HPP
+#define __CATADIOPTRIC_CUBE_MAP_HPP
+
+#include <framework.h>
+#include <opencv/cxcore.h>
+#include <CubeMapModule.hpp>
+#include <string>
+
+class CatadioptricCubeMap : public CubeMapModule
+{
+private:
+	GLuint vertexBuffer;
+	GLuint texCoordBuffer;
+	GLuint texture;
+	
+	IplImage *frame;
+	
+	float coeff[5];
+	float xc, yc;
+	
+	float width, height;
+	
+	int nbCircles, nbRays;
+	
+	std::string param_file;
+
+	void init( void* );
+	void cleanGL( void* );
+
+public:
+	Framework _framework;
+	CatadioptricCubeMap();
+	
+	void initModule();
+	void stopModule();
+	void setParameters(char *filename);
+	void inputRaw(void *data, int w, int h, int widthStep, int type);
+	
+protected:
+	virtual void pre_render();
+	virtual void render();
+};
+CLASS_AS_MODULE(CatadioptricCubeMap);
+
+#endif // __CATADIOPTRIC_CUBE_MAP_HPP
