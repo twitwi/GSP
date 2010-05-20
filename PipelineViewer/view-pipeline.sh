d818633bb8200cdbbbd671b8ba700d04dabc82d9
Rémi Emonet <remi.emonet@idiap.ch>
2010-05-20 16:09:08 +0200

    update to view-pipeline to generate intermediary svg (inkview is nicer than dotty)

--- PipelineViewer/view-pipeline.sh
+++ PipelineViewer/view-pipeline.sh
@@ -1,19 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 function getpath {
     ___p=$(pwd)
     cd $(dirname $1) && pwd && cd ${___p}
 }
 
-function xslt {
-    saxonb-xslt -xsl:$1 -s:$2 -o:$3
-
-}
-
 function usage {
     echo ""
     echo "requires saxon and dotty:"
-    echo "       sudo aptitude install libsaxonb-java graphviz"
+    echo "       sudo aptitude install libsaxonb-java graphviz inkview"
+    echo "       (libsaxonb-java is not libsaxon-java (with 'b', older))"
     echo "usage:"
     echo "       $0 mypipeline.xml"
     echo "       $0 -svg myoutput.svg mypipeline.xml"
@@ -21,18 +17,28 @@ function usage {
 }
 
 function missing {
+    msg="echo Error: $1 not found"
+    test $# -gt 1 && msg="echo $2"
     if which $1 > /dev/null
     then
         false
     else
-        echo Error: $1 not found
+        $msg
         true
     fi
 }
 
+function xslt {
+    saxon=saxonb-xslt
+    missing ${saxon} "trying saxon9he jar" && saxon="java -jar $HOME/app/saxon9he.jar"
+    ${saxon} -xsl:$1 -s:$2 -o:$3
+
+}
+
 test \( $# = 0 \) -o \( x$1 = x--help \) && usage && exit
-missing saxonb-xslt && usage && exit
+#missing saxonb-xslt && usage && exit
 missing dotty && usage && exit
+missing inkview && usage && exit
 
 exepath=$(getpath $0)
 svg=
@@ -55,8 +61,9 @@ echo "dot file is: $out"
 #cat ${out}
 if test "x$svg" = x
 then
-    dotty ${out}
-else
-    echo "svg file is: $svg"
-    dot -Tsvg -o ${svg} ${out}
+    svg="$out.svg"
 fi
+
+echo "svg file is: $svg"
+dot -Tsvg -o "${svg}" "${out}"
+inkview "${svg}"
