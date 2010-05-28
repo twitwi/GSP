20df22b53348a142df673d45126aa4283b3c5322
Rem <nomail.com>
2010-05-28 09:46:08 +0200

    added some exploration on cpp mangling

--- GSPFramework/cpp-mangling/ManySymbols.cpp
+++ GSPFramework/cpp-mangling/ManySymbols.cpp
@@ -26,3 +26,29 @@ void func015(char****************** a, char****************** b) {}
 void func016(char************************************* a, char************************************* b) {}
 void func017(char************************************** a, char************************************** b) {}
 void func018(int*i, int ii, int* iii, float* f, float ff) {}
+
+namespace NA{ // S_
+    namespace NB { // S0_
+        namespace NC { // S1_
+            class ZZZ {};
+            void func100(int*i, int ii){}
+            void func101(ZZZ*i, ZZZ**ii){} // ZZZ S2_  ; ZZZ* S3_
+            void func102(char* s, ZZZ*i, ZZZ**ii, char *s2){} // char* S2_ ; ZZZ S3_ ;  ZZZ S4_ 
+        }
+        void func110(NC::ZZZ*i, NC::ZZZ**ii){} // ZZZ S2_  ; ZZZ* S3_
+        void func112(char* s, NC::ZZZ*i, NC::ZZZ**ii, char *s2){} // char* S1_ ; NC S2_ ; ZZZ S3_ ;  ZZZ* S4_ 
+        namespace ND {
+            void func120(NC::ZZZ*i, NC::ZZZ**ii){} // ZZZ S2_  ; ZZZ* S3_
+            void func122(char* s, NC::ZZZ*i, NC::ZZZ**ii, char *s2){} // char* S2_ ; NC S3_ ; ZZZ S4_ ;  ZZZ* S5_ 
+        }
+    }
+}
+
+#include <vector>
+#include <string>
+#include <map>
+
+void func200(std::string da, std::string daa){} // Ss
+void func201(std::vector<CCC*> da, std::vector<CCC*> daa){} // !!!
+void func202(std::map<std::string, CCC*> da){} // !!!
+
