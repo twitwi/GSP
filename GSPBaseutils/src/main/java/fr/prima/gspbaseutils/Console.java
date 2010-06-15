35e296aa8fab6eb9192baaa3f773212c08c22167
Rem <nomail.com>
2010-06-15 23:03:54 +0200

    Improved Console base util to handle startup messages

--- GSPBaseutils/src/main/java/fr/prima/gspbaseutils/Console.java
+++ GSPBaseutils/src/main/java/fr/prima/gspbaseutils/Console.java
@@ -3,11 +3,43 @@ package fr.prima.gspbaseutils;
 
 import fr.prima.gsp.framework.ModuleParameter;
 import fr.prima.gsp.framework.spi.AbstractModule;
+import java.util.Formatter;
 
 public class Console extends AbstractModule {
 
     @ModuleParameter
     public int level = 10;
+
+    @ModuleParameter(initOnly=true)
+    public String init = null;
+
+    @ModuleParameter
+    public String stop = null;
+
+    private final String line = "======================================================================";
+    @ModuleParameter
+    public String format = line + "%s%n" + line + "%n";
+
+    @ModuleParameter
+    public String tab = "\n|  ";
+
+    @Override
+    protected void initModule() {
+        lifecycleMessage(init);
+    }
+
+    @Override
+    protected void stopModule() {
+        lifecycleMessage(stop);
+    }
+
+    private void lifecycleMessage(String msg) {
+        if (msg == null) {
+            return;
+        }
+        msg = tab.replaceAll("%n", "\n") + msg.replaceAll("%n", tab.replaceAll("%n", "\n"));
+        new Formatter(System.err).format(format, msg).flush();
+    }
 
     public void info(Object msg) {
         if (level > 5) System.err.println("J INFO: " + msg);
