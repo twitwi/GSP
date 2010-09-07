705c6609358635513dc6828c9d898a0387c4af7b
Rémi Emonet <remi.emonet@idiap.ch>
2010-09-07 10:18:47 +0200

    added a 'For' module that should be, in the long run, a replacement of the Clock module

---
+++ GSPBaseutils/src/main/java/fr/prima/gspbaseutils/For.java
@@ -0,0 +1,124 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.prima.gspbaseutils;
+
+import fr.prima.gsp.framework.Assembly;
+import fr.prima.gsp.framework.ModuleParameter;
+import fr.prima.gsp.framework.spi.AbstractModuleEnablable;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author twilight
+ */
+public class For extends AbstractModuleEnablable {
+
+    @ModuleParameter(initOnly = true)
+    public long warmup = 0;
+
+    @ModuleParameter(initOnly = true)
+    public long period = 0;
+
+    @ModuleParameter(initOnly = true)
+    public int from = 0;
+
+    @ModuleParameter(initOnly = true)
+    public int to = -1;
+
+    @ModuleParameter(initOnly = true)
+    public int step = 1;
+
+    /**
+     * Should the assembly be shut down when the For ends?
+     */
+    @ModuleParameter
+    public boolean stopAtEnd = true;
+
+    // this will be automatically injected by the framework
+    @ModuleParameter(initOnly = true)
+    public Assembly assembly;
+
+    private int count = 0;
+
+    Timer timer = null;
+
+    @Override
+    protected void setEnabled(boolean enabled) {
+        boolean old = this.enabled;
+        super.setEnabled(enabled);
+        if (old == this.enabled) {
+            return;
+        }
+        if (this.enabled) {
+            startTimer();
+        } else {
+            timer.cancel();
+        }
+    }
+    
+    @Override
+    protected synchronized void initModule() {
+        count = from;
+        startTimer();
+    }
+    private synchronized void startTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+        timer = new Timer("Timer Thread for the Clock GSP Module in Baseutils");
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                output();
+            }
+        }, warmup, period);
+    }
+
+    @Override
+    protected synchronized void stopModule() {
+        timer.cancel();
+    }
+
+    public void interrupt() {
+        try {
+            timer.cancel();
+            Thread.sleep(1000);
+            assembly.stop();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(For.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void output() {
+        emitEvent();
+        s();
+        i();
+        f();
+        count += step;
+        if (to != -1 && count >= to) {
+            timer.cancel();
+            if (stopAtEnd) {
+                assembly.stop();
+            }
+        }
+    }
+
+    private void s() {
+        emitEvent("" + count);
+    }
+
+    private void i() {
+        emitEvent(count);
+    }
+
+    private void f() {
+        emitEvent((float) count);
+    }
+
+}
