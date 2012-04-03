21c4d1507f0788f4b0f8236c3ff93167867b3bfa
Remi Emonet <remi.emonet@reverse--com.heeere>
2012-04-03 23:04:24 +0200

    pom: added version for the plugins + custom main change + clean run configurations

--- GSPFramework/src/main/java/fr/prima/gsp/Launcher.java
+++ GSPFramework/src/main/java/fr/prima/gsp/Launcher.java
@@ -35,7 +35,9 @@ public class Launcher {
     public static void main(String[] args) throws IOException {
         // TODO catch anything and display correct message (and localized)
         if (args.length == 0) {
-            main(new String[]{"../GSPExample/pipeline-cpp-2.xml"});
+            System.setProperty("java.library.path", "/usr/lib:/usr/lib/jni:/home/twilight/projects/GSP/GSPExample/build/cpp");
+            System.setProperty("gsp.module.path", "/usr/lib:/usr/lib/jni:/home/twilight/projects/GSP/GSPExample/build/cpp");
+            main(new String[]{"../GSPExample/test.xml"});
             if (true) return;
             main(new String[]{"pipeline-simple-with-parameter.xml", "p=100", "s=10", "fps.samples=2"});
             main(new String[]{"--help"});
