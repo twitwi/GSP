6d39310268ce2d02488f0f07c1f5eac1b67252e9
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-02-22 16:51:09 +0100

    [baseutils] cleaner StopWatch module (no tick1-10...)

--- GSPBaseutils/src/main/java/fr/prima/gspbaseutils/StopWatch.java
+++ GSPBaseutils/src/main/java/fr/prima/gspbaseutils/StopWatch.java
@@ -4,144 +4,108 @@
  */
 package fr.prima.gspbaseutils;
 
-import fr.prima.gsp.framework.spi.AbstractModuleEnablable;
+import fr.prima.gsp.framework.Event;
+import fr.prima.gsp.framework.EventReceiver;
+import fr.prima.gsp.framework.ModuleParameter;
+import fr.prima.gsp.framework.spi.AbstractModuleWithoutEventReceiver;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
- * Usually, put the connections of this module first in the pipeline file.
- * This way, the module gets called first by the framework and you measure what you want to measure.
+ * Usually, put the connections of this module first in the pipeline file. This
+ * way, the module gets called first by the framework and you measure what you
+ * want to measure.
  */
-public class StopWatch extends AbstractModuleEnablable {
+public class StopWatch extends AbstractModuleWithoutEventReceiver {
 
+    // mixing with AbstractModuleEnablable
+    @ModuleParameter(change = "enabledChanged")
+    public boolean enabled = true;
+
+    public void enabledChanged() {
+        setEnabled(enabled);
+    }
+
+    protected boolean isEnabled() {
+        return enabled;
+    }
+
+    protected void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+        emitEvent(enabled);
+    }
+    // ^ end of mixin with enableable
     long lastTick;
+    Map<String, EventReceiver> receiversCache = new HashMap<String, EventReceiver>();
+
+    public StopWatch() {
+        EventReceiver tick = new EventReceiver() {
+            public void receiveEvent(Event e) {
+                tick();
+            }
+        };
+        EventReceiver tock = new EventReceiver() {
+            public void receiveEvent(Event e) {
+                tock();
+            }
+        };
+        EventReceiver tocktick = new EventReceiver() {
+            public void receiveEvent(Event e) {
+                tocktick();
+            }
+        };
+        receiversCache.put("tick", tick);
+        receiversCache.put("tock", tock);
+        receiversCache.put("tocktick", tocktick);
+        receiversCache.put(":", tick);
+        receiversCache.put("+", tock);
+        receiversCache.put("+:", tocktick);
+    }
 
     @Override
     protected void initModule() {
         lastTick = System.currentTimeMillis();
     }
 
+    public EventReceiver getEventReceiver(final String portName) {
+        EventReceiver res = receiversCache.get(portName);
+        if (res == null) {
+            res = new EventReceiver() {
+                public void receiveEvent(Event e) {
+                    if (!enabled) {
+                        return;
+                    }
+                    System.err.println("StopWatch: unrecognized port " + portName);
+                }
+            };
+        }
+        return res;
+    }
+
     public void tick() {
+        if (!enabled) {
+            return;
+        }
         lastTick = System.currentTimeMillis();
     }
 
-    public void tick1(Object o) {
-        tick();
-    }
-
-    public void tick2(Object o1, Object o2) {
-        tick();
-    }
-
-    public void tick3(Object o1, Object o2, Object o3) {
-        tick();
-    }
-
-    public void tick4(Object o1, Object o2, Object o3, Object o4) {
-        tick();
-    }
-
-    public void tick5(Object o1, Object o2, Object o3, Object o4, Object o5) {
-        tick();
-    }
-
-    public void tick6(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6) {
-        tick();
-    }
-
-    public void tick7(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7) {
-        tick();
-    }
-
-    public void tick8(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8) {
-        tick();
-    }
-
-    public void tick9(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8, Object o9) {
-        tick();
-    }
-
     public void tock() {
+        if (!enabled) {
+            return;
+        }
         long diff = System.currentTimeMillis() - lastTick;
         output((int) diff);
     }
 
-    public void tock1(Object o) {
-        tock();
-    }
-
-    public void tock2(Object o1, Object o2) {
-        tock();
-    }
-
-    public void tock3(Object o1, Object o2, Object o3) {
-        tock();
-    }
-
-    public void tock4(Object o1, Object o2, Object o3, Object o4) {
-        tock();
-    }
-
-    public void tock5(Object o1, Object o2, Object o3, Object o4, Object o5) {
-        tock();
-    }
-
-    public void tock6(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6) {
-        tock();
-    }
-
-    public void tock7(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7) {
-        tock();
-    }
-
-    public void tock8(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8) {
-        tock();
-    }
-
-    public void tock9(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8, Object o9) {
-        tock();
-    }
-
     public void tocktick() {
+        if (!enabled) {
+            return;
+        }
         long now = System.currentTimeMillis();
         long diff = now - lastTick;
         lastTick = now;
         output((int) diff);
-    }
-
-    public void tocktick1(Object o) {
-        tocktick();
-    }
-
-    public void tocktick2(Object o1, Object o2) {
-        tocktick();
-    }
-
-    public void tocktick3(Object o1, Object o2, Object o3) {
-        tocktick();
-    }
-
-    public void tocktick4(Object o1, Object o2, Object o3, Object o4) {
-        tocktick();
-    }
-
-    public void tocktick5(Object o1, Object o2, Object o3, Object o4, Object o5) {
-        tocktick();
-    }
-
-    public void tocktick6(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6) {
-        tocktick();
-    }
-
-    public void tocktick7(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7) {
-        tocktick();
-    }
-
-    public void tocktick8(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8) {
-        tocktick();
-    }
-
-    public void tocktick9(Object o1, Object o2, Object o3, Object o4, Object o5, Object o6, Object o7, Object o8, Object o9) {
-        tocktick();
     }
 
     private void output(int diff) {
