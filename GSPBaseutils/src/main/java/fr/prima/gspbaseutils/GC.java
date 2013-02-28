8b075c7e7d6671227582537b3c96a2c77ead8933
Rémi Emonet <remi.emonet@reverse--com.heeere>
2013-02-28 11:16:40 +0100

    [baseutils] GC module now also outputs information on memory

--- GSPBaseutils/src/main/java/fr/prima/gspbaseutils/GC.java
+++ GSPBaseutils/src/main/java/fr/prima/gspbaseutils/GC.java
@@ -4,6 +4,7 @@
  */
 package fr.prima.gspbaseutils;
 
+import fr.prima.gsp.framework.ModuleParameter;
 import fr.prima.gsp.framework.spi.AbstractModuleEnablable;
 
 /**
@@ -11,9 +12,28 @@ import fr.prima.gsp.framework.spi.AbstractModuleEnablable;
  * @author remonet
  */
 public class GC extends AbstractModuleEnablable {
+
     public void input() {
         if (isEnabled()) {
             System.gc();
+            Runtime r = Runtime.getRuntime();
+            output(r.totalMemory() - r.freeMemory());
+            total(r.totalMemory());
+            free(r.freeMemory());
         }
+    }
+    @ModuleParameter
+    public double divisor = 1024. * 1024.;
+
+    private void output(long usedMemory) {
+        emitEvent(usedMemory / divisor);
+    }
+
+    private void free(long freeMemory) {
+        emitEvent(freeMemory / divisor);
+    }
+
+    private void total(long totalMemory) {
+        emitEvent(totalMemory / divisor);
     }
 }
